62f05871b7950d11507783a5d7169bdda225f939
VitBog95 <vitbog95@gmail.com>
2026-09-03 20:37:45 +0700

    Диплом: 5 API автотестов готово

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+import requests
+from config import BASE_URL, MY_TOKEN
+
+@pytest.fixture
+def auth_headers():
+    """Возвращает заголовки для авторизованных запросов"""
+    return {
+        "Cookie": f"token_global={MY_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+@pytest.fixture
+def created_lesson_id(auth_headers):
+    """Создает урок и возвращает его числовой ID"""
+    
+    payload = {
+        "backgroundColor": "#FFF7C7",
+        "color": "#FAC641",
+        "title": "Урок для теста",
+        "startAt": "2026-09-10T10:00:00+03:00",
+        "endAt": "2026-09-10T11:00:00+03:00"
+    }
+    
+    response = requests.post(
+        f"{BASE_URL}/v2/schedule/createPersonal",
+        headers=auth_headers,
+        json=payload
+    )
+
+    assert response.status_code == 200
+    json_data = response.json()
+    assert json_data.get("errors") is None
+    assert "id" in json_data["data"]["payload"]
+    
+    lesson_id = json_data["data"]["payload"]["id"]
+    print(f"✅ Фикстура создала урок с ID: {lesson_id}")
+    
+    return lesson_id
